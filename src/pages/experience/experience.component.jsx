5be685d27dc42f3e526ec313fc9ef4dafabc4fe9
sdibyanshu <sdibyanshu>
2022-10-30 23:53:54 +0530

    update

--- src/pages/experience/experience.component.jsx
+++ src/pages/experience/experience.component.jsx
@@ -35,12 +35,13 @@ function ResumeNew() {
         </Row> */}
 
         <Row className="resume">
-          <Document file={pdf} className="d-flex justify-content-center">
+          <Document style={{"width":"auto"}} file={pdf} className="d-flex justify-content-center">
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
 
         <Row style={{ justifyContent: "center", position: "relative" ,"margin-top":"20px" }}>
+
           <Button
             variant="primary"
             href={pdf}
